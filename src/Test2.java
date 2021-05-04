82d4c5314605e86d6e652b5020e6c1b446bb3843
Local Acount <Local Acount@10.198.75.52>
2021-05-04 13:30:52 -0300

    i add some input for check up

--- src/Test2.java
+++ src/Test2.java
@@ -7,6 +7,8 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("2nd is here ");
+System.out.println(" i aded 3rd is here  ");
+System.out.println(" i added 4nd is here ");
 	}
 
 }
